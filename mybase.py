714581c530cd8bd63032b332055dbd4b0af787e3
jiehunt <jiehunt@github.com>
2018-03-09 21:45:46 +0900

    add lgb funciton (1/2)

--- mybase.py
+++ mybase.py
@@ -6,15 +6,19 @@ import pandas as pd
 import sys, re, csv, codecs
 import string
 import logging
+import psutil
 
 from scipy.sparse import hstack
+from scipy.sparse import csr_matrix
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 from sklearn.metrics import roc_auc_score
 
 from keras.preprocessing import text, sequence
@@ -40,6 +44,12 @@ from keras.callbacks import Callback
 from keras.callbacks import EarlyStopping, ModelCheckpoint, LearningRateScheduler
 
 from keras.optimizers import Adam, RMSprop
+
+from contextlib import contextmanager
+
+from collections import defaultdict
+
+import lightgbm as lgb
 
 """"""""""""""""""""""""""""""
 # system setting
@@ -70,15 +80,139 @@ class RocAucEvaluation(Callback):
 
 
 """"""""""""""""""""""""""""""
-# Get Data
+# Help Function
 """"""""""""""""""""""""""""""
+# Contraction replacement patterns
+cont_patterns = [
+    (b'(W|w)on\'t', b'will not'),
+    (b'(C|c)an\'t', b'can not'),
+    (b'(I|i)\'m', b'i am'),
+    (b'(A|a)in\'t', b'is not'),
+    (b'(\w+)\'ll', b'\g<1> will'),
+    (b'(\w+)n\'t', b'\g<1> not'),
+    (b'(\w+)\'ve', b'\g<1> have'),
+    (b'(\w+)\'s', b'\g<1> is'),
+    (b'(\w+)\'re', b'\g<1> are'),
+    (b'(\w+)\'d', b'\g<1> would'),
+]
+patterns = [(re.compile(regex), repl) for (regex, repl) in cont_patterns]
+
+@contextmanager
+def timer(name):
+    """
+    Taken from Konstantin Lopuhin https://www.kaggle.com/lopuhin
+    in script named : Mercari Golf: 0.3875 CV in 75 LOC, 1900 s
+    https://www.kaggle.com/lopuhin/mercari-golf-0-3875-cv-in-75-loc-1900-s
+    """
+    t0 = time.time()
+    yield
+    print(f'[{name}] done in {time.time() - t0:.0f} s')
+
+
+def prepare_for_char_n_gram(text):
+    """ Simple text clean up process"""
+    # 1. Go to lower case (only good for english)
+    # Go to bytes_strings as I had issues removing all \n in r""
+    clean = bytes(text.lower(), encoding="utf-8")
+    # 2. Drop \n and  \t
+    clean = clean.replace(b"\n", b" ")
+    clean = clean.replace(b"\t", b" ")
+    clean = clean.replace(b"\b", b" ")
+    clean = clean.replace(b"\r", b" ")
+    # 3. Replace english contractions
+    for (pattern, repl) in patterns:
+        clean = re.sub(pattern, repl, clean)
+    # 4. Drop puntuation
+    # I could have used regex package with regex.sub(b"\p{P}", " ")
+    exclude = re.compile(b'[%s]' % re.escape(bytes(string.punctuation, encoding='utf-8')))
+    clean = b" ".join([exclude.sub(b'', token) for token in clean.split()])
+    # 5. Drop numbers - as a scientist I don't think numbers are toxic ;-)
+    clean = re.sub(b"\d+", b" ", clean)
+    # 6. Remove extra spaces - At the end of previous operations we multiplied space accurences
+    clean = re.sub(b'\s+', b' ', clean)
+    # Remove ending space if any
+    clean = re.sub(b'\s+$', b'', clean)
+    # 7. Now replace words by words surrounded by # signs
+    # e.g. my name is bond would become #my# #name# #is# #bond#
+    # clean = re.sub(b"([a-z]+)", b"#\g<1>#", clean)
+    clean = re.sub(b" ", b"# #", clean)  # Replace space
+    clean = b"#" + clean + b"#"  # add leading and trailing #
+
+    return str(clean, 'utf-8')
+
+def count_regexp_occ(regexp="", text=None):
+    """ Simple way to get the number of occurence of a regex"""
+    return len(re.findall(regexp, text))
+
+def get_indicators_and_clean_comments(df):
+    """
+    Check all sorts of content as it may help find toxic comment
+    Though I'm not sure all of them improve scores
+    """
+    # Count number of \n
+    df["ant_slash_n"] = df["comment_text"].apply(lambda x: count_regexp_occ(r"\n", x))
+    # Get length in words and characters
+    df["raw_word_len"] = df["comment_text"].apply(lambda x: len(x.split()))
+    df["raw_char_len"] = df["comment_text"].apply(lambda x: len(x))
+    # Check number of upper case, if you're angry you may write in upper case
+    df["nb_upper"] = df["comment_text"].apply(lambda x: count_regexp_occ(r"[A-Z]", x))
+    # Number of F words - f..k contains folk, fork,
+    df["nb_fk"] = df["comment_text"].apply(lambda x: count_regexp_occ(r"[Ff]\S{2}[Kk]", x))
+    # Number of S word
+    df["nb_sk"] = df["comment_text"].apply(lambda x: count_regexp_occ(r"[Ss]\S{2}[Kk]", x))
+    # Number of D words
+    df["nb_dk"] = df["comment_text"].apply(lambda x: count_regexp_occ(r"[dD]ick", x))
+    # Number of occurence of You, insulting someone usually needs someone called : you
+    df["nb_you"] = df["comment_text"].apply(lambda x: count_regexp_occ(r"\W[Yy]ou\W", x))
+    # Just to check you really refered to my mother ;-)
+    df["nb_mother"] = df["comment_text"].apply(lambda x: count_regexp_occ(r"\Wmother\W", x))
+    # Just checking for toxic 19th century vocabulary
+    df["nb_ng"] = df["comment_text"].apply(lambda x: count_regexp_occ(r"\Wnigger\W", x))
+    # Some Sentences start with a <:> so it may help
+    df["start_with_columns"] = df["comment_text"].apply(lambda x: count_regexp_occ(r"^\:+", x))
+    # Check for time stamp
+    df["has_timestamp"] = df["comment_text"].apply(lambda x: count_regexp_occ(r"\d{2}|:\d{2}", x))
+    # Check for dates 18:44, 8 December 2010
+    df["has_date_long"] = df["comment_text"].apply(lambda x: count_regexp_occ(r"\D\d{2}:\d{2}, \d{1,2} \w+ \d{4}", x))
+    # Check for date short 8 December 2010
+    df["has_date_short"] = df["comment_text"].apply(lambda x: count_regexp_occ(r"\D\d{1,2} \w+ \d{4}", x))
+    # Check for http links
+    df["has_http"] = df["comment_text"].apply(lambda x: count_regexp_occ(r"http[s]{0,1}://\S+", x))
+    # check for mail
+    df["has_mail"] = df["comment_text"].apply(
+        lambda x: count_regexp_occ(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+', x)
+    )
+    # Looking for words surrounded by == word == or """" word """"
+    df["has_emphasize_equal"] = df["comment_text"].apply(lambda x: count_regexp_occ(r"\={2}.+\={2}", x))
+    df["has_emphasize_quotes"] = df["comment_text"].apply(lambda x: count_regexp_occ(r"\"{4}\S+\"{4}", x))
+
+    # Now clean comments
+    df["clean_comment"] = df["comment_text"].apply(lambda x: prepare_for_char_n_gram(x))
+
+    # Get the new length in words and characters
+    df["clean_word_len"] = df["clean_comment"].apply(lambda x: len(x.split()))
+    df["clean_char_len"] = df["clean_comment"].apply(lambda x: len(x))
+    # Number of different characters used in a comment
+    # Using the f word only will reduce the number of letters required in the comment
+    df["clean_chars"] = df["clean_comment"].apply(lambda x: len(set(x)))
+    df["clean_chars_ratio"] = df["clean_comment"].apply(lambda x: len(set(x))) / df["clean_comment"].apply(lambda x: 1 + min(99, len(x)))
+
+def char_analyzer(text):
+    """
+    This is used to split strings in small lots
+    I saw this in an article (I can't find the link anymore)
+    so <talk> and <talking> would have <Tal> <alk> in common
+    """
+    tokens = text.split()
+    return [token[i: i + 3] for token in tokens for i in range(len(token) - 2)]
+
 
 """"""""""""""""""""""""""""""
 # Feature
 """"""""""""""""""""""""""""""
+
 def f_get_coefs(word,*arr):
   return word, np.asarray(arr, dtype='float32')
-
 
 def f_get_pretraind_features(f_train_text, f_test_text, f_embed_size, f_embedding_path, max_features = 100000, max_len = 150):
 
@@ -145,6 +279,16 @@ def f_get_tfidf_features(f_train_text, f_test_text, f_max_features=10000, f_type
                 max_df=0.9,
                 use_idf=1,
                 smooth_idf=1,
+                max_features=f_max_features,
+            )
+        elif f_type == 'tchar':
+            word_vectorizer = TfidfVectorizer(
+                sublinear_tf=True,
+                strip_accents='unicode',
+
+                tokenizer=char_analyzer,
+                ngram_range=(1, 1),
+                max_features=f_max_features,
             )
     except:
         print("exception in f_get_tfidf_features")
@@ -154,14 +298,82 @@ def f_get_tfidf_features(f_train_text, f_test_text, f_max_features=10000, f_type
         test_word_features = word_vectorizer.transform(f_test_text)
         return train_word_features, test_word_features
 
+def f_gen_tfidf_features(train, test):
+    import gc
+    class_names = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+    with timer("Performing basic NLP"):
+        get_indicators_and_clean_comments(train)
+        get_indicators_and_clean_comments(test)
 
-# f_get_tfidf_features(train['comment_text'], test['comment_text'], f_max_features = 10000, f_type = 'word')
-# print("test word over")
-# f_get_tfidf_features(train['comment_text'], test['comment_text'], f_max_features = 50000, f_type = 'char')
-# print("test char over")
-# f_get_tfidf_features(train['comment_text'], test['comment_text'], f_max_features=50000, f_type='shortchar')
-# print("test char over")
 
+    with timer("Creating numerical features"):
+        num_features = [f_ for f_ in train.columns
+                        if f_ not in ["comment_text", "clean_comment", "id", "remaining_chars",
+                                      'has_ip_address'] + class_names]
+        skl = MinMaxScaler()
+        train_num_features = csr_matrix(skl.fit_transform(train[num_features]))
+        test_num_features = csr_matrix(skl.fit_transform(test[num_features]))
+
+    # Get TF-IDF features
+    train_text = train['clean_comment']
+    test_text = test['clean_comment']
+    all_text = pd.concat([train_text, test_text])
+
+    with timer("get word features: "):
+        train_word_features, test_word_features = f_get_tfidf_features(train_text, test_text,
+        f_max_features=10000, f_type='word')
+
+    with timer("get char features: "):
+        train_char_features, test_char_features = f_get_tfidf_features(train_text, test_text,
+        f_max_features=20000, f_type='char')
+
+    with timer("get shortchar features: "):
+        train_shortchar_features, test_shortchar_features = f_get_tfidf_features(train_text, test_text,
+        f_max_features=50000, f_type='shortchar')
+
+    with timer("get tchar features: "):
+        train_tchar_features, test_tchar_features = f_get_tfidf_features(train_text, test_text,
+        f_max_features=50000, f_type='tchar')
+
+    del train_text
+    del test_text
+    gc.collect()
+
+    # Now stack TF IDF matrices
+    with timer("Staking matrices"):
+        csr_trn = hstack(
+            [
+                train_char_features,
+                train_word_features,
+                train_shortchar_features,
+                train_tchar_features,
+                train_num_features
+            ]
+        ).tocsr()
+        del train_word_features
+        del train_num_features
+        del train_char_features
+        del train_tchar_features
+        del train_shortchar_features
+        gc.collect()
+
+        csr_sub = hstack(
+            [
+                test_char_features,
+                test_word_features,
+                test_shortchar_features,
+                test_tchar_features,
+                test_num_features
+            ]
+        ).tocsr()
+        del test_word_features
+        del test_num_features
+        del test_char_features
+        del test_tchar_features
+        del test_shortchar_features
+        gc.collect()
+
+    return csr_trn, csr_sub
 
 
 """"""""""""""""""""""""""""""
@@ -236,6 +448,27 @@ def m_lstm_model(m_max_len, m_max_features, m_embed_size, m_embedding_matrix,
                         verbose = m_verbose, callbacks = [ra_val, check_point, early_stop])
     model = load_model(m_file_path)
     return model
+
+def m_lgb_model(train, test):
+
+    # Set LGBM parameters
+    params = {
+        "objective": "binary",
+        'metric': {'auc'},
+        "boosting_type": "gbdt",
+        "verbosity": -1,
+        "num_threads": 4,
+        "bagging_fraction": 0.8,
+        "feature_fraction": 0.8,
+        "learning_rate": 0.1,
+        "num_leaves": 31,
+        "verbose": -1,
+        "min_split_gain": .1,
+        "reg_alpha": .1,
+        # "device": "gpu",
+        # "gpu_platform_id": 0,
+        # "gpu_device_id": 0
+    }
 
 
 
@@ -322,10 +555,15 @@ def app_rnn (train, test):
     m_make_single_submission(m_infile, m_outfile, m_pred)
     return
 
+def app_lbg (train, test):
+    with timer ("gen tfidf features"):
+        f_gen_tfidf_features(train, test)
 
-print ("goto app_rnn")
-app_rnn(train, test)
+# print ("goto app_rnn")
+# app_rnn(train, test)
 
+print ("goto tfidf")
+app_lbg(train, test)
 
 """"""""""""""""""""""""""""""
 # Stacking
